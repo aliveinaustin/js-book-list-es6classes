5642ae5a71274b927ceb3951b2d003c74985ef6b
Thomas Hays <aliveinaustin@gmail.com>
2018-01-03 16:58:18 -0600

    "Added Local Storage"

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-Basic JS OOP Book List App using ES6 Classes instead of ES5 Prototypes.
+Basic JS OOP Book List App (with Local Storage) using ES6 Classes instead of ES5 Prototypes.
 
-- Add new books (title, author, ISBN#) to a browser list (no Local Storage)
+- Add new books (title, author, ISBN#) to list in UI and Local Storage
 - Remove books from list by clicking an X icon
 - Show error msg if input fields are left blank
 - Show success msg when books are added or removed from list
--- appes6.js
+++ appes6.js
@@ -53,6 +53,52 @@ class UI {
   }
 }
 
+// Local Storage Class
+class Store {
+  static getBooks() {
+    let books;
+    if(localStorage.getItem('books')  === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+  }
+  static displayBooks() {
+    const books = Store.getBooks();
+
+    books.forEach(function(book) {
+      const ui = new UI;
+
+      // Add book to UI
+      ui.addBookToList(book);
+
+    });
+  }
+
+  static addBook(book){
+    const books = Store.getBooks();
+
+    books.push(book);
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+
+    books.forEach(function(book, index) {
+      if(book.isbn === isbn) {
+        books.splice(index, 1)
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+// DOM Load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
 // Event Listener for adding book
 document.getElementById('book-form').addEventListener('submit', function(e) {
   // Get form values
@@ -76,6 +122,9 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
   // Add book to list
   ui.addBookToList(book);
 
+  // Add to LS
+  Store.addBook(book);
+
   // Show success
   ui.showAlert('New book added.', 'success');
 
@@ -91,7 +140,11 @@ document.getElementById('book-list'). addEventListener('click', function(e){
   // Instantiate UI Object
   const ui = new UI();
 
+  //Delete book
   ui.deleteBook(e.target);
+
+  // Remove from LS
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
   // Show alert
   ui.showAlert('Book Removed!', 'success');
